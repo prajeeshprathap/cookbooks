7cc4d7862ccaf3aa8ddc50d4b31f07e41f9a41b1
Prajeesh Prathap <prajeesh.prathap@gmail.com>
2015-08-12 06:37:37 +0200

    added chocholatey source to the Package Management

--- chocolatey/recipes/configure_repo.rb
+++ chocolatey/recipes/configure_repo.rb
@@ -1,1 +1,7 @@
 chocolatey 'PowerShell'
+
+powershell_script 'add_new_chocolatey_source' do
+  code <<-EOH
+  Register-PackageSource –provider Chocolatey –name AzureChoco –location http://prajeeshchoco.azurewebsites.net/nuget
+  EOH
+end
