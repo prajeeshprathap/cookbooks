95fc4a06372914bd4043d7bd4492018aeeb05e35
Prajeesh Prathap <prajeesh.prathap@gmail.com>
2015-08-13 06:46:17 +0200

    Added new cookbook

--- chocolatey/recipes/configure_repo.rb
+++
@@ -1,7 +0,0 @@
-chocolatey 'PowerShell'
-
-powershell_script 'add_new_chocolatey_source' do
-  code <<-EOH
-  Register-PackageSource –provider Chocolatey –name AzureChoco –location http://prajeeshchoco.azurewebsites.net/nuget
-  EOH
-end
