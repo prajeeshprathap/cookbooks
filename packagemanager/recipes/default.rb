629a9d3a5fc2a2528ba4af173a4d5b46b2f52964
Prajeesh Prathap <prajeesh.prathap@gmail.com>
2015-08-14 06:56:11 +0200

    Added pre attribute to powershell installation

--- packagemanager/recipes/default.rb
+++ packagemanager/recipes/default.rb
@@ -2,12 +2,14 @@
 # Cookbook Name:: packagemanager
 # Recipe:: default
 #
-# Copyright 2015, PRAJEESH PRATHAO
+# Copyright 2015, PRAJEESH PRATHAp
 #
 # All rights reserved - Do Not Redistribute
 #
 include_recipe 'chocolatey'
-chocolatey 'PowerShell'
+chocolatey 'PowerShell' do
+	args "-pre"
+end
 
 powershell_script 'Add Chocolatey Source' do
   code <<-EOH
