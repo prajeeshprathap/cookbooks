0e8fe79f18cc2fdd98e886ceeb97c4705c192e16
Prajeesh Prathap <prajeesh.prathap@gmail.com>
2015-08-17 07:44:03 +0200

    updated the packagemanager recipie to download resources from github

--- packagemanager/recipes/default.rb
+++ packagemanager/recipes/default.rb
@@ -11,6 +11,34 @@ chocolatey 'PowerShell' do
 	args "-pre"
 end
 
+dsc_resource 'ChocolateySourceDef' do
+	resource :xGithubModule
+	property :url, 'https://raw.githubusercontent.com/prajeeshprathap/DSCResources/master/xChocolateySource/xChocolateySource.psd1'
+	property :module, 'xChocolateySource'
+	property :ensure, 'Present'
+end
+
+dsc_resource 'ChocolateySourceModule' do
+	resource :xGithubModule
+	property :url, 'https://raw.githubusercontent.com/prajeeshprathap/DSCResources/master/xChocolateySource/xChocolateySource.psm1'
+	property :module, 'xChocolateySource'
+	property :ensure, 'Present'
+end
+
+dsc_resource 'PackageManagementDef' do
+	resource :xGithubModule
+	property :url, 'https://raw.githubusercontent.com/prajeeshprathap/DSCResources/master/xPackageManagement/xPackageManagement.psd1'
+	property :module, 'xPackageManagement'
+	property :ensure, 'Present'
+end
+
+dsc_resource 'PackageManagementModule' do
+	resource :xGithubModule
+	property :url, 'https://raw.githubusercontent.com/prajeeshprathap/DSCResources/master/xPackageManagement/xPackageManagement.psm1'
+	property :module, 'xPackageManagement'
+	property :ensure, 'Present'
+end
+
 dsc_resource 'AzureChocolateySource' do
 	resource :xChocolateySource
 	property :name, 'AzureChoco'
